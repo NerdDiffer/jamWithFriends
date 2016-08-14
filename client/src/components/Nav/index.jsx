538dedb357b91f2291d0fa7055ebbd5877654e30
Rafael Espinoza <nerddiffer@gmail.com>
2016-08-14 15:58:54 -0700

    Simplify NavBar & update styles

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -2,23 +2,29 @@ import AppBar from 'material-ui/AppBar';
 import React from 'react';
 import { Link } from 'react-router';
 
-const color={ backgroundImage: 'url("http://bit.ly/2b2ePzs")', width: "100%", opacity: 0.6 };
+const styles = {
+  backgroundColor: 'Peru'
+};
+
+const Icon = () => (
+  <i className="material-icons">music_note</i>
+);
 
 const AppNavBar = () => (
-
   <div id="navBar">
     <AppBar
-      style={color}
-      showMenuIconButton={false}
+      title={'tbd'}
+      style={styles}
+      iconElementLeft={<Icon />}
+      onTitleTouchTap={() => console.log('Go home')}
     >
-      <Link to="/">
-        <img id="logo" src="http://bit.ly/2beSCQg" />
+      <Link to="login" >
+        <button className="navButtons">Login</button>
       </Link>
-      <Link to="login" ><button className="navButtons"> Login!</button>
+      <Link to="signup">
+        <button className="navButtons">Signup</button>
       </Link>
-      <Link to="signup"><button className="navButtons"> Signup!</button>
-      </Link>
-    </AppBar >
+    </AppBar>
   </div>
 );
 
