ccd4bc72aca07d5dcc0eb203cd4cb80f4498f69c
Rafael Espinoza <nerddiffer@gmail.com>
2016-08-26 16:27:58 -0700

    Remove paragraph

--- client/src/components/About/index.jsx
+++ client/src/components/About/index.jsx
@@ -16,17 +16,6 @@ const aboutAuthorsStyle = {
 
 const About = (props) => (
   <div className="about">
-    <Paper style={aboutAppStyles}>
-      <section>
-        <p>LiveJam allows music aficianados to collaboratively jam out using virtual instruments.</p>
-        <p>The app lets “Jammers” form or join dedicated rooms where they can create tracks (and lifetime memories) with one another.</p>
-        <p>LiveJam’s rooms currently allow for up to four Jammers to collab per session.</p>
-        <p>The app's use of direct peer-to-peer (serverless) communication, via WebRTC, allows users within 100 miles of one another to jam without lag, emulating the experience of using a real studio.</p>
-      </section>
-    </Paper>
-
-    <hr />
-
     <Paper style={aboutAuthorsStyle}>
       <section className="group">
         <p>Colossal Bubble is a group of developers based in San Francisco, CA.</p>
