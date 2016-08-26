b93d68c40a0ec97eb581d7be0fea4bb7a554c307
Rafael Espinoza <nerddiffer@gmail.com>
2016-08-26 11:49:57 -0700

    Scaffold out additional sections for About

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,29 +1,39 @@
 import React from 'react';
 import Paper from 'material-ui/Paper';
 
-const paperStyle = {
-  height: 300,
+const aboutAppStyles = {
   width: 400,
-  margin: 20,
-  textAlign: 'center',
-  display: 'inline-block',
-  backgroundImage: 'url("http://bit.ly/2b2ePzs")',
-  borderRadius: '12%',
-  opacity: 0.6,
-  padding: '5px',
-  fontWeight: 900,
-  fontFamily: '"Trebuchet MS", Helvetica, sansSerif'
+  margin: '0 auto',
+  padding: '1%'
+};
+
+const aboutAuthorsStyle = {
+  margin: '0 auto',
+  padding: '1%',
 };
 
 const About = (props) => (
-  <Paper style={paperStyle} zDepth={5} >
-    <div id="introText">
-      <p>LiveJam allows music aficianados to collaboratively jam out using virtual instruments.</p>
-      <p>The app lets “Jammers” form or join dedicated rooms where they can create tracks (and lifetime memories) with one another.</p>
-      <p>LiveJam’s rooms currently allow for up to four Jammers to collab per session.</p>
-      <p>The app's use of direct peer-to-peer (serverless) communication, via WebRTC, allows users within 100 miles of one another to jam without lag, emulating the experience of using a real studio.</p>
-    </div>
-  </Paper>
+  <div className="about">
+    <Paper style={aboutAppStyles}>
+      <section>
+        <p>LiveJam allows music aficianados to collaboratively jam out using virtual instruments.</p>
+        <p>The app lets “Jammers” form or join dedicated rooms where they can create tracks (and lifetime memories) with one another.</p>
+        <p>LiveJam’s rooms currently allow for up to four Jammers to collab per session.</p>
+        <p>The app's use of direct peer-to-peer (serverless) communication, via WebRTC, allows users within 100 miles of one another to jam without lag, emulating the experience of using a real studio.</p>
+      </section>
+    </Paper>
+
+    <hr />
+
+    <Paper style={aboutAuthorsStyle}>
+      <section className="group">
+      </section>
+      <section className="authors">
+        <section className="author">
+        </section>
+      </section>
+    </Paper>
+  </div>
 );
 
 export default About;
